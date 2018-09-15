579e9d38884f4192f8389f7503b5067658eee49c
cyrusclarke <cyrusjclarke@gmail.com>
2018-09-16 01:48:41 +0200

    trying to recover

--- app/controllers/uploadfiles_controller.rb
+++ app/controllers/uploadfiles_controller.rb
@@ -4,5 +4,14 @@ class UploadfilesController < ApplicationController
 		@uploadfile = Uploadfile.new
 	end
 
-	
+	def create
+		@uploadfile = current_user.uploadfiles.create(uploadfile_params)
+		redirect_to uploadfile_path(@uploadfile)
+	end
+
+	private
+
+	def uploadfile_params
+	    params.require(:uploadfile).permit(:title, :subtitle, :video)
+	end
 end
--- app/models/uploadfile.rb
+++ app/models/uploadfile.rb
@@ -1,4 +1,4 @@
 class Uploadfile < ApplicationRecord
 	belongs_to :user
-	mount_uploader :genericfiles, GenericfilesUploader
+	mount_uploader :video, VideoUploader
 end
