be1219c66ce56f5d380060c5766bd8492ebdecaa
cyrusclarke <cyrusjclarke@gmail.com>
2018-09-19 18:52:18 +0200

    add my account

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :uploadfiles, only: [:new, :create, :show]
+  resources :users, only: :show
 
 end
